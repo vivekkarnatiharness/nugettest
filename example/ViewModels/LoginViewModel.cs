ae0fa63f9f66458c959de596ab44c22aa4471863
vivekkarnatiharness <vivek.karnati@harness.io>
2023-04-11 22:46:07 +0530

    ss

---
+++ example/ViewModels/LoginViewModel.cs
@@ -0,0 +1,46 @@
+﻿using PasswordBoxMVVM.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PasswordBoxMVVM.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new LoginCommand(this);
+        }
+    }
+}
